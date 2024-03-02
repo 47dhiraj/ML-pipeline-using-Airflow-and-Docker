12c8a11f8d96d24d30d5c468beb180b75c2caacc
dhirajkafle553 <dhirajkafle553@gmail.com>
2024-03-02 19:27:12 +0545

    ml pipeline code updated to latest airflow

--- airflow/dags/ml_pipeline.py
+++ airflow/dags/ml_pipeline.py
@@ -1,12 +1,13 @@
+from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
-from airflow import DAG
 
-from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
 from airflow.operators.bash import BashOperator
+
+from airflow.decorators import task
 from airflow.providers.docker.operators.docker import DockerOperator
 
 import pandas as pd
-
 import pickle
 
 
@@ -18,13 +19,12 @@ default_args = {
     'depends_on_past': False,                  
     'email_on_failure': False,
     'email_on_retry': False,
-    # 'retries': 1,                            
-    # 'retry_delay': timedelta(minutes=1),              
+    'start_date': days_ago(1),         
 }
 
 
 
-# function to train model
+@task
 def train_recommendation_model(**kwargs):
     
     # Loading .csv datasets
@@ -42,17 +42,15 @@ def train_recommendation_model(**kwargs):
 
     print('Recommendation model trained successfully !!')
 
-    ## Pushing data between task
     item_similarity_df_pkl_file = '/tmp/item_similarity_df.pkl'
     item_similarity_df.to_pickle(item_similarity_df_pkl_file)
-    kwargs['task_instance'].xcom_push(key='item_similarity_df_pkl_file', value=item_similarity_df_pkl_file)
+    
+    return item_similarity_df_pkl_file
 
 
 
-
-def test_recommendation_model(**kwargs):
-
-    item_similarity_df_pkl_file = kwargs['task_instance'].xcom_pull(task_ids='recommendation_model_training', key='item_similarity_df_pkl_file')
+@task
+def test_recommendation_model(item_similarity_df_pkl_file):
 
     if item_similarity_df_pkl_file is None:
         print("Error: item_similarity_df_pkl_file is None")
@@ -60,7 +58,6 @@ def test_recommendation_model(**kwargs):
     else:
         print("Received item_similarity_df_pkl_file:", item_similarity_df_pkl_file)
 
-        ## Loading the DataFrame 
         try:
             item_similarity_df = pd.read_pickle(item_similarity_df_pkl_file)
         except Exception as e:
@@ -85,28 +82,27 @@ def test_recommendation_model(**kwargs):
 
         print('Recommendation model tested successfully !!')
 
-        ## Pushing data between task
+
         all_recommend_dict = all_recommend.to_dict()
-        kwargs['task_instance'].xcom_push(key='all_recommend', value=all_recommend_dict)
+
+        return all_recommend_dict
 
 
 
-
-def tune_recommendation_model(**kwargs):
-
-    all_recommend_dict = kwargs['task_instance'].xcom_pull(task_ids='recommendation_model_testing', key='all_recommend')
+@task
+def tune_recommendation_model(all_recommend_dict):
 
     if all_recommend_dict is None:
         print("Error: all_recommend_dict is None")
         return
     else:
         print("Received all_recommend_dict:", all_recommend_dict)
-
+    
         all_recommend = pd.Series(all_recommend_dict)
 
         check_name = ['The Martian']
 
-        # Checking & eliminating, if the recommended movies is already seen ??
+        # Checking & eliminating, if the recommended movies is already seen.
         def check_seen(movie, seen_movies):
             for item in seen_movies:
                 if item == movie:
@@ -120,7 +116,6 @@ def tune_recommendation_model(**kwargs):
         for movie, score in all_recommend.items():
             if not check_seen(movie, check_name):
                 print(movie,'\t\t\t\t\t',  score)
-                # print(movie)
 
             i = i + 1
             if i >= 36 + len(check_name):
@@ -129,42 +124,16 @@ def tune_recommendation_model(**kwargs):
 
 
 
-
-# Creating/Declaring a DAG object
+# Creating / Declaring a DAG object
 ml_dag = DAG(
     dag_id='ml_pipeline_dag',                         
     default_args=default_args,        
     description='ML pipeline for Recommendation System',
-    start_date=datetime(2024, 2, 26),
+    schedule_interval=None,
     catchup=False,
 )
 
 
-# Task to train the recommendation model)
-model_train_task = PythonOperator(
-    task_id='recommendation_model_training',     
-    python_callable=train_recommendation_model,         
-    dag=ml_dag,
-)
 
-
-# Task to test the recommendation model)
-model_test_task = PythonOperator(
-    task_id='recommendation_model_testing',     
-    python_callable=test_recommendation_model,
-    provide_context=True,     
-    dag=ml_dag,
-)
-
-
-# Task too tune the recommendation model for better performance
-model_tune_task = PythonOperator(
-    task_id='recommendation_model_tuning',     
-    python_callable=tune_recommendation_model,
-    provide_context=True,     
-    dag=ml_dag,
-)
-
-
-# Setting Task execution order
-model_train_task >> model_test_task >> model_tune_task
+with ml_dag:
+    load_task = tune_recommendation_model(test_recommendation_model(train_recommendation_model()))
